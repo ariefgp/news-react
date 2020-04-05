4f81c35732c04925c4248dcd88a83e9c6adee3da
Arief Gusti <arief@ariefstudio.com>
2020-04-05 21:41:54 +0700

    change news to ID

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
 
-AppRegistry.registerComponent('royal_news', () => App);
+AppRegistry.registerComponent('news-init', () => App);
--- src/news.js
+++ src/news.js
@@ -1,5 +1,5 @@
 const url =
-  "https://newsapi.org/v2/top-headlines?country=us&apiKey=6e3019eb1c2f4cdbbde62d0d0ef05d48";
+  "https://newsapi.org/v2/top-headlines?country=id&apiKey=6e3019eb1c2f4cdbbde62d0d0ef05d48";
 
 export async function getNews() {
   let result = await fetch(url).then(response => response.json());
