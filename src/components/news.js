a921e6642e21694e1367d9c10ef8da3fbcb6acc2
Arief Gusti <arief@ariefstudio.com>
2020-04-05 14:59:31 +0700

    change api key

--- src/components/news.js
+++ src/components/news.js
@@ -1,5 +1,5 @@
 const url =
-  "https://newsapi.org/v2/top-headlines?country=us&apiKey=YOUR_API_KEY_HERE";
+  "https://newsapi.org/v2/top-headlines?country=us&apiKey=6e3019eb1c2f4cdbbde62d0d0ef05d48";
 
 export async function getNews() {
   let result = await fetch(url).then(response => response.json());
