57ceb756f8bf2594b2cb1f29863a74db1addf0f3
Arief Gusti <arief@ariefstudio.com>
2020-04-05 14:58:58 +0700

    add news component

---
+++ src/components/news.js
@@ -0,0 +1,7 @@
+const url =
+  "https://newsapi.org/v2/top-headlines?country=us&apiKey=YOUR_API_KEY_HERE";
+
+export async function getNews() {
+  let result = await fetch(url).then(response => response.json());
+  return result.articles;
+}
